05e055b8b48762ce9a60390933c67f1598a83ec5
AbdeRaoufDaiffiDZ <raoufdaiffi@gmail.com>
2025-05-09 15:05:06 +0200

    add email register confirming

--- models/customerModel.js
+++ models/customerModel.js
@@ -23,6 +23,10 @@ const customerSchema = new Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      default: 'customer'
+  }
   },
   { timestamps: true }
 );
--- routes/home/homeRoutes.js
+++ routes/home/homeRoutes.js
@@ -3,11 +3,12 @@ const homeControllers = require("../../controllers/home/homeControllers");
 router.get("/get-categorys", homeControllers.get_categorys); // done
 router.get("/get-products", homeControllers.get_products); // done
 router.get("/get-product/:slug", homeControllers.get_product); // done
-router.get("/price-range-latest-product", homeControllers.price_range_product);
-router.get("/query-products", homeControllers.query_products);
+router.get("/price-range-latest-product", homeControllers.price_range_product); // done
+router.get("/query-products", homeControllers.query_products); // from flutter is working good, problem in the backend
 
-router.post("/customer/submit-review", homeControllers.submit_review);
+router.post("/customer/submit-review", homeControllers.submit_review); // done
 router.get("/customer/get-reviews/:productId", homeControllers.get_reviews);
 router.get("/get-active-guides", homeControllers.get_active_guides);
+router.post("/chat-boot", homeControllers.chatBoot);
 
 module.exports = router;
